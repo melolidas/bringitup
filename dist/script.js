cb996922e1f97d4304de24e48985767e8189f053
theHalid <the_halidcastle@mail.ru>
2024-11-18 02:05:14 +0600

    john Smith sliding up feature done

--- dist/script.js
+++ dist/script.js
@@ -26,6 +26,18 @@ class Slider {
     if (n < 1) {
       this.slideIndex = this.slides.length;
     }
+    try {
+      this.hanson.style.opacity = "0";
+      if (n === 3) {
+        this.hanson.classList.add("animated");
+        setTimeout(() => {
+          this.hanson.style.opacity = "1";
+          this.hanson.classList.add("slideInUp");
+        }, 1000);
+      } else {
+        this.hanson.classList.remove("slideInUp");
+      }
+    } catch (e) {}
     Array.from(this.slides).forEach(slide => {
       slide.style.display = "none";
     });
@@ -37,6 +49,9 @@ class Slider {
     this.showSlides(this.slideIndex += n);
   }
   render() {
+    try {
+      this.hanson = document.querySelector(".hanson");
+    } catch (e) {}
     if (this.page && this.btns.length > 0) {
       this.btns.forEach(item => {
         item.addEventListener("click", () => {
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -14,6 +14,18 @@ export default class Slider {
     if (n < 1) {
       this.slideIndex = this.slides.length;
     }
+    try {
+      this.hanson.style.opacity = "0";
+      if (n === 3) {
+        this.hanson.classList.add("animated");
+        setTimeout(() => {
+          this.hanson.style.opacity = "1";
+          this.hanson.classList.add("slideInUp");
+        }, 1000);
+      } else {
+        this.hanson.classList.remove("slideInUp");
+      }
+    } catch (e) {}
 
     Array.from(this.slides).forEach((slide) => {
       slide.style.display = "none";
@@ -29,6 +41,9 @@ export default class Slider {
   }
 
   render() {
+    try {
+      this.hanson = document.querySelector(".hanson");
+    } catch (e) {}
     if (this.page && this.btns.length > 0) {
       this.btns.forEach((item) => {
         item.addEventListener("click", () => {
